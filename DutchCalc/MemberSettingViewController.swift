d3ab9df1c9ab9aa4d2edf5de639fc5aa157312bb
Tori <ttang1135@naver.com>
2023-02-08 15:31:33 +0900

    인원 설정 페이지 레이아웃

---
+++ DutchCalc/MemberSettingViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MemberSettingViewController.swift
+//  DutchCalc
+//
+//  Created by 방유빈 on 2023/02/08.
+//
+
+import UIKit
+
+class MemberSettingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
