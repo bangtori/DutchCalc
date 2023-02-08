208ac91f8cf1a624310dffb16633991fc79ad267
Tori <ttang1135@naver.com>
2023-02-08 15:42:02 +0900

    인원설정페이지_ 설명label 설정

--- DutchCalc/MemberSettingViewController.swift
+++ DutchCalc/MemberSettingViewController.swift
@@ -9,10 +9,19 @@ import UIKit
 
 class MemberSettingViewController: UIViewController {
 
+    @IBOutlet weak var explanationLabel: UILabel!
+    @IBOutlet weak var inputTextField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        initExplabationLabel()
 
         // Do any additional setup after loading the view.
     }
-
+    func initExplabationLabel(){
+        explanationLabel.text = "중간에 참여하거나, 나간 인원을 포함한 모든 인원을 추가해주세요."
+        explanationLabel.lineBreakMode = .byCharWrapping
+        explanationLabel.numberOfLines = 0
+    }
 }
